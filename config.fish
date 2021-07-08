af84504d456e451a55acbbaa31054e1d0755ee58
NKJM-R <nakajima.ryota28jp@gmail.com>
2021-07-09 01:17:43 +0900

    Add: 更新

--- config.fish
+++ config.fish
@@ -11,10 +11,50 @@ if status is-interactive
   bind -M insert \cn history-search-forward
   bind -M insert \cp history-search-backward
   
-  bind -M default \cwv tmux_split_window_v
-  bind -M default \cws tmux_split_window_s
+  bind -M default -m default \csl tmux_session_list
+  bind -M default -m default \css tmux_session_search
+  bind -M default -m default \csn tmux_session_new
+
+  bind -M default -m default \cwv tmux_split_window_v
+  bind -M default -m default \cws tmux_split_window_s
+
+  bind -M default -m default \cww tmux_select_pane_next
+  bind -M default -m default \cwh tmux_select_pane_left
+  bind -M default -m default \cwj tmux_select_pane_down
+  bind -M default -m default \cwk tmux_select_pane_up
+  bind -M default -m default \cwl tmux_select_pane_right
+
+  bind -M default -m default \cw\cw tmux_select_pane_next
+  bind -M default -m default \cw\ch tmux_select_pane_left
+  bind -M default -m default \cw\cj tmux_select_pane_down
+  bind -M default -m default \cw\ck tmux_select_pane_up
+  bind -M default -m default \cw\cl tmux_select_pane_right
 
   source ~/.config/dotfiles/.aliasrc 
+end
+
+function tmux_session_list
+  tmux ls
+end
+
+function tmux_select_pane_next
+  tmux select-pane -t +
+end
+
+function tmux_select_pane_left
+  tmux select-pane -L
+end
+
+function tmux_select_pane_down
+  tmux select-pane -D
+end
+
+function tmux_select_pane_up
+  tmux select-pane -U
+end
+
+function tmux_select_pane_right
+  tmux select-pane -R
 end
 
 function tmux_split_window_s
@@ -23,4 +63,13 @@ end
 
 function tmux_split_window_v
   tmux split-window -h
+end
+
+function tmux_session_search
+  tmux list-sessions -F \"[##{session_windows}] ##S\" | fzf-tmux | sed 's/^\[[0-9]*\] //' | xargs tmux switch -t
+end
+
+function tmux_session_new
+  read SESSION_NAME
+  tmux new -s $SESSION_NAME
 end
