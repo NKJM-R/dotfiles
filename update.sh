dcd9c4f5f8fac6d9fff239a23e4550cbc74863ac
git-eg74-nakajima <nakajima.ryota.eg74@members.co.jp>
2021-07-12 10:54:01 +0900

    Add: update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+DOTFILES="${HOME}/.config/dotfiles"
+
+# fish
+echo "source ${DOTFILES}/config.fish" > ~/.config/fish/config.fish
+
+# tmux
+cat "${DOTFILES}/.tmux.conf" > ~/.tmux.conf
+
+# alacritty
+cat "${DOTFILES}/alacritty.yml" > ~/.config/alacritty/alacritty.yml
